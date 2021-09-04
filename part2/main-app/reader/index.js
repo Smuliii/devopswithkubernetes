1ad1ecb428e6ec4068de0d3c8babb655a7bd080f
Smuliii <smulitus@gmail.coms>
2021-09-04 23:46:27 +0300

    Add ex 2.03-2.06

--- part2/main-app/reader/index.js
+++ part2/main-app/reader/index.js
@@ -6,6 +6,7 @@ import got from 'got';
 
 const dir = process.env.FILE_PATH;
 const hashFile = path.join(dir, 'hash.txt');
+const message = process.env.MESSAGE;
 const app = express();
 const port = 3001;
 
@@ -19,7 +20,7 @@ const readHash = async () => {
 	try {
 		const hash = await fs.promises.readFile(hashFile, 'utf-8');
 		const pingpongs = await got(process.env.PINGPONG_URL).text();
-		return hash + '<br>' + 'Ping / Pongs: ' + pingpongs;
+		return message + '<br>' + hash + '<br>' + 'Ping / Pongs: ' + pingpongs;
 	} catch (e) {
 		console.log(e);
 		return 'file(s) not found';
